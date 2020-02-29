5df95342008fe3666fe68b96449d1508c01a1446
Frederick Articuno <mckiyoshi@gmail.com>
2020-02-28 22:15:07 -0500

    Drink Api Works, Youtube mysteriously broken

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# MakeADrink
+# MakeADrink - Web application using api 1/5
+
+This web app will use https://thecocktaildb.com's API to find drinks then feed the results to https://youtube-search.p.rapidapi.com to find related youtube videos.
+
+
+## [Try it yourself!](https://fkarticuno.github.io/MakeADrink/) 
+
+![image](https://fkarticuno.github.io/CodingPortfolio/Assets/Images/22.png)
+
+## Built With
+* [Bootstrap](https://getbootstrap.com/docs/4.4/getting-started/introduction/) - An open source toolkit for developing with HTML, CSS, and JS.
+* [HTML](https://en.wikipedia.org/wiki/HTML) - Standard markup language for documents designed to be displayed in a web browser.
+* [JavaScript](https://www.javascript.com/) - High-level, often just-in-time compiled, and multi-paradigm.
+* [Jquery](https://jquery.com/) - A fast, small, and feature-rich JavaScript library
+* [GoogleFonts](https://fonts.google.com/?selection.family=Akronim) - Fancy fonts to make site unique.
+
+
+## Authors
+
+See the list of [contributors](https://github.com/fkarticuno/MakeADrink/graphs/contributors) who participated in this project along with individual commit history. 
+
+## Planned updates
+
+New revisions of Portfolio will contain these example apps to showcase my skills in development
+1. [Web application using api](https://github.com/fkarticuno/MakeADrink)
+2. [Brochure site](https://github.com/fkarticuno/ChipsRestaurant)
+3. [E-commerce site](https://github.com/fkarticuno/ShopAround)
+4. [Personal Portfolio](https://github.com/fkarticuno/CodingPortfolio)
+5. [Simple game](https://github.com/fkarticuno/JSpiano)
---
+++ script.js
@@ -0,0 +1,102 @@
+var searchQuery;
+$(document).ready(function () {
+    $('#drinkSearchBtn').on('click', function () {
+        dumpDivs()
+        searchQuery = $('#drinkSearch').val().trim();
+        console.log(searchQuery);
+        getDrinkInfo('search.php?s=' + searchQuery);
+    })
+    $('#randomDrinkBtn').on('click', function () {
+        dumpDivs()
+        searchQuery = 'random.php';
+        console.log(searchQuery);
+        getDrinkInfo(searchQuery);
+    })
+
+});
+function dumpDivs() {
+    $('#cocktailName').html(" ")
+    $('#image').html(" ")
+    $('#ingredients').html(" ")
+    $('#instructions').html(" ")
+}
+function getDrinkInfo(param) {
+    url = 'https://www.thecocktaildb.com/api/json/v1/1/' + param;
+    response = $.getJSON(url, function (data) {
+        console.log('data', data)
+        if (data.drinks != null) {
+            drinkData = data.drinks[0]
+            // ============
+            ing = [];
+            mea = [];
+            ings = [];
+            meas = [];
+            ing.push(drinkData.strIngredient1, drinkData.strIngredient2, drinkData.strIngredient3, drinkData.strIngredient4,
+                drinkData.strIngredient5, drinkData.strIngredient6, drinkData.strIngredient7, drinkData.strIngredient8,
+                drinkData.strIngredient9, drinkData.strIngredient10, drinkData.strIngredient11, drinkData.strIngredient12,
+                drinkData.strIngredient13, drinkData.strIngredient14, drinkData.strIngredient15
+            )
+            mea.push(drinkData.strMeasure1, drinkData.strMeasure2, drinkData.strMeasure3, drinkData.strMeasure4,
+                drinkData.strMeasure5, drinkData.strMeasure6, drinkData.strMeasure7, drinkData.strMeasure8,
+                drinkData.strMeasure9, drinkData.strMeasure10, drinkData.strMeasure11, drinkData.strMeasure12,
+                drinkData.strMeasure13, drinkData.strMeasure14, drinkData.strMeasure15
+            )
+            sliceNulls(ing, mea);
+
+            function sliceNulls(ingArr, meaArr) {
+                for (var i = 0; i < ingArr.length; i++) {
+                    if (ingArr[i] != null) {
+                        ings.push(ingArr[i]);
+                        meas.push(meaArr[i])
+                    }
+                }
+            }
+            drinkId = drinkData.idDrink;
+            name = drinkData.strDrink;
+            image = drinkData.strDrinkThumb;
+            instructions = drinkData.strInstructions;
+            ingredients = ings;
+            measurements = meas;
+            // ============
+            $("#cocktailName").append(`<div class="data" data="${drinkId}">${name}</div>`);
+            $("#image").append(`<img src="${image}"/>`);
+            for (var i = 0; i < ings.length; i++) {
+                if (ings[i] != null){
+                    $("#ingredients").append(`<span>${meas[i]} ${ings[i]} </span>`);
+                }
+            };
+            $("#instructions").append(`<p>${instructions}</p>`);
+            searchVideo(name)
+            $('#contentName').attr("name", "cocktailName");
+            $('#contentName').attr("value", name);
+        } else {
+            $('#content').append(`<div class='data'>Nothing Found</div>`);
+        };
+    })
+}
+// ========================
+function searchVideo(qry) {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://youtube-search.p.rapidapi.com/search?q=${qry}%20recipe&part=snippet&key=AIzaSyAOsteuaW5ifVvA_RkLXh0mYs6GLAD6ykc`,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "youtube-search.p.rapidapi.com",
+            "x-rapidapi-key": "1bb40a3f2cmsh737e756f49ff562p1d4980jsn6537251a9bc1"
+        }
+    }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        $('#content2').html(`
+        <iframe src="https://www.youtube.com/embed/${response.items[0].id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        `);
+    });
+};
+$(document).ready( // this may run before searchVideo()
+    $('#drinkSearchBtn').on('click', function (event) {
+        event.preventDefault();
+        var input = $('#drinkSearch').val().trim();
+        searchVideo(input)
+    })
+);
